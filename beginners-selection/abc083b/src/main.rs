690f40154a2711bd01da24e0e8d35a773dda4963
Daiki48 <daiki48.engineer@gmail.com>
2023-09-30 01:00:14 +0900

    :zap: Success

--- beginners-selection/abc083b/src/main.rs
+++ beginners-selection/abc083b/src/main.rs
@@ -18,16 +18,15 @@ fn main() {
     let b: i32 = nab.next().unwrap().trim().parse().unwrap();
 
     let mut ans: i32 = 0;
-    if (1..=10i32.pow(4)).contains(&n) && (1..b).contains(&a) && (a + 1..=36).contains(&b) {
+    if (1..=10i32.pow(4)).contains(&n) && (1..=36).contains(&a) && (a..=36).contains(&b) {
         for i in 1..=n {
-            let result = sum_digits(i);
-            // println!("{} : {}", i, &result);
+            let result: i32 = sum_digits(i);
             if (a..=b).contains(&result) {
                 ans += i;
             }
         }
+        println!("{}", ans);
     } else {
         println!("Invalid input");
     }
-    println!("{}", ans);
 }
