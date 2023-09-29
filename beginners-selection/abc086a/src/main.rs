3ed5b291eab8700b30f31260bc39f1693f85a191
Daiki48 <daiki48.engineer@gmail.com>
2023-09-29 11:15:20 +0900

    :wrench: Success

--- beginners-selection/abc086a/src/main.rs
+++ beginners-selection/abc086a/src/main.rs
@@ -6,8 +6,8 @@ fn main() {
     let mut ab = ab.split_whitespace();
     let a: i32 = ab.next().unwrap().parse().unwrap();
     let b: i32 = ab.next().unwrap().parse().unwrap();
-    if 1 <= a && a <= 10000 && 1 <= b && b <= 10000 {
-        println!("{} {}", a, b);
+    if (1..=10000).contains(&a) && (1..=10000).contains(&b) {
+        // println!("{} {}", a, b);
         let result = a * b;
         if result % 2 == 0 {
             println!("Even");
